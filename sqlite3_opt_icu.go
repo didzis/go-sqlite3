875c394aa4999567caf563b4a6b92ee389ce2bcf
Didzis Gosko <didzis@users.noreply.github.com>
2026-02-23 19:53:15 +0200

    Update ICU library list for Linux and link against static ICU libraries on Darwin

--- sqlite3_opt_icu.go
+++ sqlite3_opt_icu.go
@@ -9,12 +9,15 @@
 package sqlite3
 
 /*
-#cgo LDFLAGS: -licuuc -licui18n
 #cgo CFLAGS: -DSQLITE_ENABLE_ICU
+#cgo linux LDFLAGS: -licudata -licui18n -licuio -licutest -licutu -licuuc
+
 #cgo darwin,amd64 CFLAGS:  -I/usr/local/opt/icu4c/include
-#cgo darwin,amd64 LDFLAGS: -L/usr/local/opt/icu4c/lib
+#cgo darwin,arm64 LDFLAGS: /opt/homebrew/opt/icu4c/lib/libicudata.a /opt/homebrew/opt/icu4c/lib/libicui18n.a /opt/homebrew/opt/icu4c/lib/libicuio.a /opt/homebrew/opt/icu4c/lib/libicutest.a /opt/homebrew/opt/icu4c/lib/libicutu.a /opt/homebrew/opt/icu4c/lib/libicuuc.a
+
 #cgo darwin,arm64 CFLAGS:  -I/opt/homebrew/opt/icu4c/include
-#cgo darwin,arm64 LDFLAGS: -L/opt/homebrew/opt/icu4c/lib
+#cgo darwin,amd64 LDFLAGS: /usr/local/opt/icu4c/lib/libicudata.a /usr/local/opt/icu4c/lib/libicui18n.a /usr/local/opt/icu4c/lib/libicuio.a /usr/local/opt/icu4c/lib/libicutest.a /usr/local/opt/icu4c/lib/libicutu.a /usr/local/opt/icu4c/lib/libicuuc.a
+
 #cgo openbsd LDFLAGS: -lsqlite3
 */
 import "C"
